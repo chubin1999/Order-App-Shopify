5ba10b139c7dbdc17d28600c49aa2d2b5bfecd42
Phuc <phucnguyen.convertdigital@gmail.com>
2023-06-28 18:26:33 +0700

    Fisrt commit

---
+++ web/frontend/components/ProductsCard.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import { Card, TextContainer, Text } from "@shopify/polaris";
+import { Toast } from "@shopify/app-bridge-react";
+import { useTranslation } from "react-i18next";
+import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+
+export function ProductsCard() {
+  const emptyToastProps = { content: null };
+  const [isLoading, setIsLoading] = useState(false);
+  const [toastProps, setToastProps] = useState(emptyToastProps);
+  const fetch = useAuthenticatedFetch();
+  const { t } = useTranslation();
+  const productsCount = 5;
+
+  /*const {
+    data,
+    refetch: refetchProductCount,
+    isLoading: isLoadingCount,
+    isRefetching: isRefetchingCount,
+  } = useAppQuery({
+    url: "/api/products/count",
+    reactQueryOptions: {
+      onSuccess: () => {
+        setIsLoading(false);
+      },
+    },
+  });*/
+
+  const {
+    data,
+    refetch: refetchProductCount,
+    isLoading: isLoadingCount,
+    isRefetching: isRefetchingCount,
+  } = useAppQuery({
+    url: "/api/orders",
+    reactQueryOptions: {
+      onSuccess: () => {
+        setIsLoading(false);
+      },
+    },
+  });
+
+  console.log(data)
+
+  const toastMarkup = toastProps.content && !isRefetchingCount && (
+    <Toast {...toastProps} onDismiss={() => setToastProps(emptyToastProps)} />
+  );
+
+  const handlePopulate = async () => {
+    setIsLoading(false);
+    const response = await fetch("/api/products/create");
+
+    if (response.ok) {
+      await refetchProductCount();
+      setToastProps({
+        content: t("ProductsCard.productsCreatedToast", {
+          count: productsCount,
+        }),
+      });
+    } else {
+      setIsLoading(false);
+      setToastProps({
+        content: t("ProductsCard.errorCreatingProductsToast"),
+        error: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      {toastMarkup}
+      <Card
+        title={t("ProductsCard.title")}
+        sectioned
+      >
+        <TextContainer spacing="loose">
+          <Text as="h4" variant="headingMd">
+            {t("ProductsCard.totalProductsHeading")}
+            <Text variant="bodyMd" as="p" fontWeight="semibold">
+              Orders: {data}
+            </Text>
+          </Text>
+        </TextContainer>
+      </Card>
+    </>
+  );
+}
---
+++ web/frontend/pages/index.jsx
@@ -0,0 +1,57 @@
+import {
+  Card,
+  Page,
+  Layout,
+  TextContainer,
+  Image,
+  Stack,
+  Link,
+  Text,
+} from "@shopify/polaris";
+import { TitleBar } from "@shopify/app-bridge-react";
+import { useTranslation, Trans } from "react-i18next";
+
+import { trophyImage } from "../assets";
+
+import { ProductsCard } from "../components";
+
+export default function HomePage() {
+  const { t } = useTranslation();
+  return (
+    <Page narrowWidth>
+      <TitleBar title={t("HomePage.title")} primaryAction={null} />
+      <Layout>
+        <Layout.Section>
+          <Card sectioned>
+            <Stack
+              wrap={false}
+              spacing="extraTight"
+              distribution="trailing"
+              alignment="center"
+            >
+              <Stack.Item fill>
+                <TextContainer spacing="loose">
+                  <Text as="h2" variant="headingMd">
+                    {t("HomePage.heading")}
+                  </Text>
+                </TextContainer>
+              </Stack.Item>
+              <Stack.Item>
+                <div style={{ padding: "0 20px" }}>
+                  <Image
+                    source={trophyImage}
+                    alt={t("HomePage.trophyAltText")}
+                    width={120}
+                  />
+                </div>
+              </Stack.Item>
+            </Stack>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <ProductsCard />
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+}
